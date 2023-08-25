5f7544e01bce910980a43ecee96467cee4eabb75
Sakif Ul Alam Saim <93915978+SakifSaim@users.noreply.github.com>
2023-08-25 23:29:20 +0600

    1455

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -6,24 +6,24 @@ import { useState } from 'react'
 function Map() {
   const navigate = useNavigate()
   const [searchParams, setSearchParams] = useSearchParams()
-  const mapPosition = useState([40, 0])
+  const [mapPosition, setMapPosition] = useState([40, 0])
 
   const lat = searchParams.get('lat')
   const lng = searchParams.get('lng')
 
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        navigate('form')
-      }}
-    >
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        className={styles.map}
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={mapPosition}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
