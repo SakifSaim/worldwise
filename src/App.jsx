51a8aa9a037929599e04898caece85131f4be079
Sakif Ul Alam Saim <93915978+SakifSaim@users.noreply.github.com>
2023-08-30 18:19:56 +0600

    loginloagout

--- src/App.jsx
+++ src/App.jsx
@@ -11,34 +11,37 @@ import CountryList from './components/CountryList'
 import City from './components/City'
 import Form from './components/Form'
 import { CitiesProvider } from './contexts/CitiesContext'
+import { AuthProvider } from './contexts/AuthContext'
 
 // for manual json data inpu
 // import city from '../data/cities.json'
 
 function App() {
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Homepage />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="login" element={<Login />} />
-          <Route path="app" element={<AppLayout />}>
-            {/* for auto opening a page on time of loading  Navigate props is used  */}
-            <Route index element={<Navigate replace to={'cities'} />} />
-            <Route path="cities" element={<CityList />} />
-            <Route path="cities/:id" element={<City />} />
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="login" element={<Login />} />
+            <Route path="app" element={<AppLayout />}>
+              {/* for auto opening a page on time of loading  Navigate props is used  */}
+              <Route index element={<Navigate replace to={'cities'} />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
 
-            <Route path="countries" element={<CountryList />} />
+              <Route path="countries" element={<CountryList />} />
 
-            <Route path="form" element={<Form />} />
-          </Route>
+              <Route path="form" element={<Form />} />
+            </Route>
 
-          <Route path="/*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+            <Route path="/*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider>
   )
 }
 
